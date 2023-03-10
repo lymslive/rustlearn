d814c4d1e03ef956e6e0bcaefbc060c48a312d3d
lymslive <403708621@qq.com>
2023-03-10 09:16:53 +0800

    refactor: extract unify sample.toml

--- worktoml/tomloper/examples/pathread.rs
+++ worktoml/tomloper/examples/pathread.rs
@@ -5,23 +5,7 @@ use tomloper::PathOperator;
 
 fn main()
 {
-    let str_toml = r#"
-ip = "127.0.0.1"
-[host]
-ip = "127.0.1.1"
-port = 8080
-protocol = ["tcp", "udp", "mmp"]
-[[service]]
-name = "serv_1"
-desc = "first server"
-[[service]]
-name = "serv_2"
-desc = "another server"
-[misc]
-int = 1234
-float = 3.14
-bool = true
-"#;
+    let str_toml = include_str!("./sample.toml");
     let v: toml::Value = str_toml.parse().unwrap();
 
     println!("original toml content:");
